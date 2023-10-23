1ff75e20234c797f72bc2755e00ecd98e6d5da8f
Ranapriyanshi <112609820+Ranapriyanshi@users.noreply.github.com>
2023-10-23 16:16:42 +0530

    chore:Designed the home page

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inria+Sans:wght@300;400;500;700&display=swap',
+        },
+      ],
+    }
+  },
+}
+</script>
